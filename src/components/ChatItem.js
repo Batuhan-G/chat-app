1254644a04b26d3bf5395369e92cec587bba1413
batuhangoren99@hotmail.com <batuhangoren99@hotmail.com>
2022-11-01 12:38:46 +0300

    Transmission of messages

--- src/components/ChatItem.js
+++ src/components/ChatItem.js
@@ -2,7 +2,7 @@ import style from './style.module.css'
 
 function ChatItem({ item }) {
   return (
-    <div className={style.chatItem}>{item.messages}</div>
+    <div className={`${style.chatItem} ${item.fromMe ? style.right : ''}`}>{item.message}</div>
   )
 }
 
--- src/components/Container.js
+++ src/components/Container.js
@@ -1,18 +1,23 @@
-import {useEffect} from 'react'
+import { useEffect } from 'react'
 import ChatForm from './ChatForm'
 import ChatList from './ChatList'
-import {init} from "../socketApi"
+import { useChat } from "../context/ChatContext"
+import { init, subscribeChat } from "../socketApi"
 
 function Container() {
-
+  const {setMessages} = useChat()
   useEffect(() => {
     init()
-  },[])
+
+    subscribeChat((message) => {
+      setMessages((prevState) => [...prevState, { message }])
+    })
+  }, [setMessages])
 
   return (
     <div className='App'>
-        <ChatList />
-        <ChatForm />
+      <ChatList />
+      <ChatForm />
     </div>
   )
 }
